eacddd91bcf3d61cbbcd7100bfec8cc43f43ba25
Alex SatuRn <pr.alexandr@mail.ru>
2019-03-01 02:02:07 +0300

    Репозиторий для организаций.

---
+++ src/main/java/WebApp/repository/OrganizationsRepository.java
@@ -0,0 +1,11 @@
+package WebApp.repository;
+
+
+import WebApp.entity.Organizations;
+import org.springframework.data.repository.CrudRepository;
+
+public interface OrganizationsRepository extends CrudRepository<Organizations, Long> {
+
+
+
+}
