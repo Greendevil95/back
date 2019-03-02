0db6a9c00e2d07c25fd1134efd31e2949f971a65
Alex SatuRn <pr.alexandr@mail.ru>
2019-03-02 18:01:59 +0300

    Обновил репозиторий

--- src/main/java/WebApp/repository/UserRepository.java
+++ src/main/java/WebApp/repository/UserRepository.java
@@ -7,8 +7,6 @@ import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<User,Long> {
     void deleteById(Long id);
-    User findById(Integer id);
     Optional<User> findByEmail(String email);
     void deleteByEmail(String email);
-
 }
