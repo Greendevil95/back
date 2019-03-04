93e72b7a5c27d4c0acdc8cec0213da9b358f46c5
Alex SatuRn <pr.alexandr@mail.ru>
2019-03-04 04:24:20 +0300

    upbate organization service

--- src/main/java/WebApp/service/OrganizatioService.java
+++ src/main/java/WebApp/service/OrganizatioService.java
@@ -1,10 +1,31 @@
 package WebApp.service;
 
 import WebApp.entity.Organization;
+import WebApp.entity.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public interface OrganizatioService {
-    ResponseEntity addOrganization(Long id, Organization organization);
+
+    ResponseEntity add(Long id, Organization organization);
+
+
+    ResponseEntity<Iterable<Organization>> getAll();
+
+    ResponseEntity<Optional<Organization>> getAllForUser(User user);
+
+    ResponseEntity<Organization> getById(Long id);
+
+
+    ResponseEntity updateById(Long id, Organization organization);
+
+    ResponseEntity update(Organization organization);
+
+
+    ResponseEntity deleteById(Long id);
+
+    ResponseEntity delete(Organization organization);
 }
--- src/main/java/WebApp/service/OrganizationServiceImpl.java
+++ src/main/java/WebApp/service/OrganizationServiceImpl.java
@@ -20,12 +20,70 @@ public class OrganizationServiceImpl implements OrganizatioService {
     private OrganizationRepository organizationRepository;
 
     @Override
-    public ResponseEntity addOrganization(Long id, Organization organization) {
+    public ResponseEntity add(Long id, Organization organization) {
         Optional user =userRepository.findById(id);
         if (user.isPresent()){
             organization.setUser((User) user.get());
             organizationRepository.save(organization);
             return ResponseEntity.ok("Organization with name "+ organization.getName()+ " added for user with id "+ ((User) user.get()).getId());
+        }else   return ResponseEntity.notFound().build();
+    }
+
+    @Override
+    public ResponseEntity<Iterable<Organization>> getAll() {
+        return ResponseEntity.ok(organizationRepository.findAll());
+    }
+
+    @Override
+    public ResponseEntity<Optional<Organization>> getAllForUser(User user) {
+        if (userRepository.findByEmail(user.getEmail()).isPresent()){
+            Long id = userRepository.findByEmail(user.getEmail()).get().getId();
+            if (organizationRepository.findById(id).isPresent()){
+                return ResponseEntity.ok(organizationRepository.findById(id));
+            }else    return ResponseEntity.notFound().build();
+        }else   return ResponseEntity.notFound().build();
+    }
+
+    @Override
+    public ResponseEntity<Organization> getById(Long id){
+        if(organizationRepository.findById(id).isPresent()){
+            return ResponseEntity.ok(organizationRepository.findById(id).get());
+        }else   return ResponseEntity.notFound().build();
+    }
+
+    @Override
+    public ResponseEntity updateById(Long id, Organization organization) {
+        if (organizationRepository.findById(id).isPresent()){
+            organization.setId(id);
+            organizationRepository.save(organization);
+            return ResponseEntity.ok("Organization with id "+ organization.getId() + " and name " + organization.getName() + " was update.");
+        }else   return ResponseEntity.notFound().build();
+    }
+
+    @Override
+    public ResponseEntity update(Organization organization) {
+        Long orgId=organization.getId();
+        if (organizationRepository.findById(orgId).isPresent()){
+            organizationRepository.save(organization);
+            return ResponseEntity.ok("Organization with id "+ organization.getId() + " and name " + organization.getName() + " was update.");
+        }else   return ResponseEntity.notFound().build();
+    }
+
+    @Override
+    public ResponseEntity deleteById(Long id) {
+        if (organizationRepository.findById(id).isPresent()){
+            organizationRepository.deleteById(id);
+            return ResponseEntity.ok("Organization with id "+ id + "was delete.");
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @Override
+    public ResponseEntity delete(Organization organization) {
+        Long orgId = organization.getId();
+        if (organizationRepository.findById(orgId).isPresent()){
+            organizationRepository.deleteById(orgId);
+            return ResponseEntity.ok("Organization with id "+ orgId + "was delete.");
         }
         return ResponseEntity.notFound().build();
     }
