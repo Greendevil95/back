6857a58f5b73f85f6f15d8c16699ca803cd24aa6
Alex SatuRn <pr.alexandr@mail.ru>
2019-03-05 06:02:17 +0300

    update all Controllers for generic

--- src/main/java/WebApp/controller/OrganizationController.java
+++ src/main/java/WebApp/controller/OrganizationController.java
@@ -2,55 +2,24 @@ package WebApp.controller;
 
 import WebApp.entity.Organization;
 import WebApp.service.OrganizatioService;
+import WebApp.service.OrganizationServiceImpl;
 import WebApp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/organization")
-public class OrganizationController {
+public class OrganizationController extends AbstractController<Organization,OrganizationServiceImpl> {
+
+    public OrganizationController(OrganizationServiceImpl service) {
+        super(service);
+    }
 
     @Autowired
     OrganizatioService organizatioService;
 
     @Autowired
     UserService userService;
-
-    @GetMapping("")
-    public ResponseEntity<Iterable<Organization>> getAllOrganization() {
-        return organizatioService.getAll();
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<Organization> getOrganization(@PathVariable(value = "id") Long Id) {
-        return organizatioService.getById(Id);
-    }
-
-    @PutMapping
-    public ResponseEntity updateOrganization(@RequestBody Organization organization) {
-        return organizatioService.update(organization);
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity updateOrganizationById(@PathVariable(value = "id") Long id, @RequestBody Organization organization){
-        return organizatioService.updateById(id,organization);
-    }
-
-    @DeleteMapping
-    public ResponseEntity deleteOrganization(@RequestBody Organization organization){
-        return organizatioService.delete(organization);
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity deleteOrganizationById(@PathVariable(value = "id") Long id){
-        return organizatioService.deleteById(id);
-    }
-
-
-
-
-
-
 
 }
--- src/main/java/WebApp/controller/UserController.java
+++ src/main/java/WebApp/controller/UserController.java
@@ -4,13 +4,22 @@ import WebApp.entity.Organization;
 import WebApp.entity.User;
 import WebApp.service.OrganizatioService;
 import WebApp.service.UserService;
+import WebApp.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
-public class UserController {
+public class UserController extends AbstractController<User,UserServiceImpl> {
+
+    public UserController(UserServiceImpl service) {
+        super(service);
+    }
+
+
+
+
 
     @Autowired
     private UserService userService;
@@ -18,18 +27,9 @@ public class UserController {
     @Autowired
     OrganizatioService organizatioService;
 
-    @GetMapping("")
-    public ResponseEntity<Iterable<User>> getAllUsers() {
-        return userService.getAll();
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<User> getUser(@PathVariable(value = "id") Long Id) {
-        return userService.getById(Id);
-    }
 
     @PostMapping
-    public ResponseEntity addUser(@RequestBody User user) {
+    public ResponseEntity add(@RequestBody User user) {
         return userService.add(user);
     }
 
@@ -37,25 +37,4 @@ public class UserController {
     public ResponseEntity addOrganization(@PathVariable(value = "id") Long id, @RequestBody Organization organization ){
         return organizatioService.add(id,organization);
     }
-
-    @PutMapping
-    public ResponseEntity updateUser(@RequestBody User user) {
-        return userService.update(user);
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity updateUserById(@PathVariable(value = "id") Long id, @RequestBody User user){
-        return userService.updateById(id,user);
-    }
-
-    @DeleteMapping
-    public ResponseEntity deleteUser(@RequestBody User user){
-        return userService.delete(user);
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity deleteUserById(@PathVariable(value = "id") Long id){
-        return userService.deleteById(id);
-    }
-
 }
