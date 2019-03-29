2f5840ebb09057f4b101d66a5f556bfc0bb5401e
Alex SatuRn <pr.alexandr@mail.ru>
2019-03-29 16:12:59 +0300

    fix reservation repo

--- src/main/java/WebApp/repository/ReservationRepository.java
+++ src/main/java/WebApp/repository/ReservationRepository.java
@@ -1,6 +1,9 @@
 package WebApp.repository;
 
+import WebApp.entity.Organization;
 import WebApp.entity.Reservation;
 
 public interface ReservationRepository extends CommonRepository<Reservation> {
+    Iterable<Reservation> findByOrganization (Organization organization);
+    Integer countByOrganization (Organization organization);
 }
