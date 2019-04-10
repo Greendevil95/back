a0d360318d07ac52d296bd3d14d2b58e5e8e35bb
Alex SatuRn <pr.alexandr@mail.ru>
2019-04-10 20:50:51 +0300

    fix repo for sort and page

--- src/main/java/WebApp/repository/UserRepository.java
+++ src/main/java/WebApp/repository/UserRepository.java
@@ -3,7 +3,9 @@ package WebApp.repository;
 import WebApp.entity.Organization;
 import WebApp.entity.State;
 import WebApp.entity.User;
+import org.springframework.data.domain.Pageable;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends CommonRepository<User> {
@@ -11,5 +13,5 @@ public interface UserRepository extends CommonRepository<User> {
     Optional<User> findByEmail(String email);
     void deleteByEmail(String email);
     Optional<User> findByOrganization (Organization organization);
-    Iterable<User> findByStates(State state);
+    List<User> findByStates(State state, Pageable pageable);
 }
