a1645b5ecd2f95747731533ef048b57f57dfcee0
Alex SatuRn <pr.alexandr@mail.ru>
2019-04-10 20:49:55 +0300

    fix secured

--- src/main/java/WebApp/security/config/WebSecurityConfig.java
+++ src/main/java/WebApp/security/config/WebSecurityConfig.java
@@ -4,11 +4,9 @@ import WebApp.security.Details.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -44,5 +42,4 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.formLogin().disable();
         http.logout().disable();
     }
-
 }
