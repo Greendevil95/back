8b9ef057e8f59489ca1af573296a0d29d636a999
Denis <Denbond95@mail.ru>
2019-04-13 22:56:41 +0300

    Fix getAuth

--- src/main/java/WebApp/security/WebSecurityConfig.java
+++ src/main/java/WebApp/security/WebSecurityConfig.java
@@ -37,5 +37,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().antMatchers(HttpMethod.POST, "/users");
+        web.ignoring().antMatchers(HttpMethod.POST, "/organizations");
+
     }
 }
