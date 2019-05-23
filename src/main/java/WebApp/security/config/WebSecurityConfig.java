f34cfaf3fd605fcd43984cde3e7436f91dfdffc9
Alex SatuRn <pr.alexandr@mail.ru>
2019-05-23 22:10:55 +0300

    fix security

--- src/main/java/WebApp/security/config/WebSecurityConfig.java
+++ src/main/java/WebApp/security/config/WebSecurityConfig.java
@@ -38,8 +38,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().httpBasic();
-        http.formLogin().disable();
-        http.logout().disable();
+        http.csrf().disable()
+                .httpBasic().disable()
+                .formLogin().disable()
+                .logout().disable();
     }
 }
