0bc614a24c926a9d66f555857f3589da9c9617f3
Alex SatuRn <pr.alexandr@mail.ru>
2019-05-24 15:37:59 +0300

    add Report controller fix and Report service fix

--- src/main/java/WebApp/service/ReportService.java
+++ src/main/java/WebApp/service/ReportService.java
@@ -1,8 +1,18 @@
 package WebApp.service;
 
 import WebApp.entity.Report;
+import WebApp.entity.User;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface ReportService extends CommonService<Report> {
+
+    ResponseEntity setStatusById(Long id, Boolean status);
+
+    ResponseEntity<User> getUserByReportId(Long id);
+
+    ResponseEntity<User> getOwnerOrganizationByReportId(Long id);
+
+    ResponseEntity<WebApp.entity.Service> getServiceByReportId(Long id);
 }
